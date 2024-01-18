23209e58896c6a4cb568c4dc0573b5a76ef5eabe
NoScriptNick <144457708+NoScriptNick@users.noreply.github.com>
2024-01-18 09:45:19 -0500

    dfghsfh

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,16 +1,39 @@
+import jdk.nashorn.internal.ir.WhileNode;
+
 import java.util.Scanner;
 public class TicTacToe {
 
     private static final int ROW = 3;
     private static final int COL = 3;
+    private static final String [][] board = new String[ROW][COL];
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        clearBoard();
+        String playerOne = "";
+        String playerTwo = "";
+        boolean done = false;
+        String temp;
 
-        //You will need to create two players and assign them "X" or "O" as their symbol for the game.
-        //At the beginning of each game, the board should be cleared (set each position to "-" to show the spot is empty).
+
+        while (!done) {
+            temp = InputHelper.getNonZeroLenString(scan, "Player 1, choose X or O");
+            if (temp.equalsIgnoreCase("X")) {
+                playerOne = "X";
+                playerTwo = "O";
+                done = true;
+            } else if (temp.equalsIgnoreCase("O")) {
+                playerOne = "O";
+                playerTwo = "X";
+                done = true;
+            } else {
+                System.out.println("Invalid");
+            }
+        }
+
+        clearBoard();
+        displayBoard();
+
         //When asking each player for their move, ask them for a row [1-3] and column [1-3]. These values will need to be shifted since the indices start at 0.
         //Your InputHelper methods should be utilized to ensure that valid values within the given range are collected.
         //Any time a player makes a move, your program should check to see if a win or tie has occurred. Keep in mind that you will write multiple methods to check for different types of wins.
@@ -21,23 +44,30 @@ public class TicTacToe {
     }
 
     private static void clearBoard(){
-        String [][] board = new String[3][3];
         for (int r = 0; r<board.length; r++) {
             for (int c = 0; c<board[0].length; c++) {
-                System.out.println(board[r][c] + " - ");
+                board[r][c] = "-";
             }
         }
     }
 
     private static void displayBoard(){
-        //shows the Tic Tac Toe game board in its current state
-        //Should appear as a 3x3 grid
-
+        for (int r = 0; r < board.length; r++) {
+            for (int c = 0; c < board[0].length; c++) {
+                System.out.print(board[r][c] + " ");
+            }
+            System.out.println();
+        }
     }
 
     private static boolean isValidMove(int row, int col){
         //returns true if there is a space at the given proposed move coordinates which means it is a legal move
         //Returns false otherwise
+        boolean valid = false;
+        if (row > 0 && row < 4 && col > 0 && col < 4) {
+            valid = true;
+        }
+        return valid;
     }
 
     private static boolean isWin(String player){
@@ -60,4 +90,5 @@ public class TicTacToe {
     private static boolean isTie(){
         //checks for a tie condition: all spaces on the board are filled and no win has occurred
     }
+
 }
